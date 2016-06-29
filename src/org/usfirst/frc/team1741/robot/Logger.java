b6180768d8077627a9fd6465447ace3837db5e68
RandomTestFive <randomtestfive@gmail.com>
2016-06-29 15:18:26 -0400

    Fix Logger class

--- src/org/usfirst/frc/team1741/robot/Logger.java
+++ src/org/usfirst/frc/team1741/robot/Logger.java
@@ -1,108 +1,116 @@
 package org.usfirst.frc.team1741.robot;
 
-import java.io.FileOutputStream;
-import java.util.Vector;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Map.Entry;
 
 public class Logger 
 {
-//	private String filename;
-//	private FileOutputStream m_log;
-//	private Vector< std::pair<String, String> > m_fields;
-//
-//	Logger()
-//	{
-//		
-//	}
-//	
-//	boolean Open(String filename) 
-//	{
-//		this.filename = filename;
-//		m_log = new FileOutputStream(filename, true);
-//		return m_log.is_open() && m_log.good();
-//	}
-//
-//	boolean Close() 
-//	{
-//		m_log.close();
-//		return !m_log.is_open();
-//	}
-//
-//	boolean Reset() 
-//	{
-//		Close();
-//		Open(this.filename);
-//		WriteAttributes();
-//		return true;
-//	}
-//
-//	boolean HasAttribute(String name) 
-//	{
-//		return (FindField(name) != -1);
-//	}
-//
-//	// TODO: needs some serious optimization most likely
-//	int FindField(String name) 
-//	{
-//		String real_name = Normalize(name);
-//		for (unsigned int i = 0; i < m_fields.size(); ++i) {
-//			if (real_name == m_fields[i].first) {
-//				return i;
-//			}
-//		}
-//		return -1;
-//	}
-//
-//
-//	boolean AddAttribute(String field) 
-//	{
-//		if (HasAttribute(field)) {
-//			// TODO: Output warning
-//			return false; // We already have this attribute
-//		}
-//
-//		std::pair<std::string, std::string> item;
-//		item.first = Normalize(field);
-//		item.second = "";
-//
-//		m_fields.push_back(item);
-//
-//		return true;
-//	}
-//
-//	boolean Log(String field, float data) {
-//		std::ostringstream ostr;
-//		ostr << data;
-//		String str(ostr.str());
-//		return Log(field, str);
-//	}
-//
-//	boolean Log(String field, String data) {
-//		String converted_field = Normalize(field);
-//		int idx = FindField(converted_field);
-//		if (idx < 0) return false;
-//
-//		m_fields[idx].second = data;
-//		return true;
-//	}
-//
-//	boolean WriteAttributes() {
-//		for (unsigned int i = 0; i < m_fields.size(); ++i) {
-//			m_log << m_fields[i].first << ',';
-//		}
-//		m_log << std::endl; // use \n if this is too slow
-//		return m_log.good();
-//	}
-//
-//	boolean WriteLine() {
-//		for (unsigned int i = 0; i < m_fields.size(); ++i) {
-//			m_log << m_fields[i].second << ',';
-//		}
-//		m_log << std::endl;
-//		return m_log.good();
-//	}
-//
-//	String Normalize(String str) 
-//	{
-//		return str.toLowerCase();
-//	}
+	private String filename;
+	private PrintWriter m_log;
+	private ArrayList< Entry<String, String> > m_fields;
+
+	Logger()
+	{
+		m_fields = new ArrayList<Entry<String,String>>();
+	}
+
+	boolean Open(String filename)
+	{
+		this.filename = filename;
+		try
+		{
+			m_log = new PrintWriter(filename);
+		}
+		catch (FileNotFoundException e)
+		{
+			return false;
+		}
+		return true;
+	}
+
+	void Close()
+	{
+		m_log.close();
+	}
+
+	boolean Reset()
+	{
+		Close();
+		Open(this.filename);
+		WriteAttributes();
+		return true;
+	}
+
+	boolean HasAttribute(String name)
+	{
+		return (FindField(name) != -1);
+	}
+
+	// TODO: needs some serious optimization most likely
+	int FindField(String name)
+	{
+		String real_name = Normalize(name);
+		for (Entry<String,String> e : m_fields)
+		{
+			if (real_name == e.getKey())
+			{
+				return m_fields.indexOf(e);
+			}
+		}
+		return -1;
+	}
+
+	boolean AddAttribute(String field)
+	{
+		if (HasAttribute(field)) {
+			// TODO: Output warning
+			return false; // We already have this attribute
+		}
+
+		m_fields.add(new AbstractMap.SimpleEntry<String,String>(Normalize(field), ""));
+
+		return true;
+	}
+
+	boolean Log(String field, float data)
+	{
+		return Log(field, String.valueOf(data));
+	}
+
+	boolean Log(String field, String data)
+	{;
+		int idx = FindField(field);
+		if (idx < 0) return false;
+
+		m_fields.get(idx).setValue(data);
+		return true;
+	}
+
+	boolean WriteAttributes()
+	{
+		for (Entry<String,String> e : m_fields)
+		{
+			m_log.print(e.getKey() + ',');
+		}
+		m_log.println();
+		return !m_log.checkError();
+	}
+
+	boolean WriteLine()
+	{
+		for (Entry<String,String> e : m_fields)
+		{
+			m_log.print(e.getValue() + ',');
+		}
+		m_log.println();
+		return !m_log.checkError();
+	}
+
+	String Normalize(String str)
+	{
+		return str.toLowerCase();
+	}
 }
