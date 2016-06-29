9f8cabcd91530d8793a0bfe6c0fb261372570747
RandomTestFive <randomtestfive@gmail.com>
2016-06-29 19:34:19 -0400

    Include PIDSourceType

--- src/org/usfirst/frc/team1741/robot/Drive.java
+++ src/org/usfirst/frc/team1741/robot/Drive.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team1741.robot;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDSourceType;
 
 public class Drive 
 {
