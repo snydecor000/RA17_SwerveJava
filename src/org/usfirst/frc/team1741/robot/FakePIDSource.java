c5da9355f156dd0e5d80c28e7e8cd419a0d32cb5
snydecor000 <snydecor000@students.centergrove.k12.in.us>
2016-07-01 16:24:28 -0400

    Added fakePIDsource in order to make offsets for the absolute encoders a thing

---
+++ src/org/usfirst/frc/team1741/robot/FakePIDSource.java
@@ -0,0 +1,84 @@
+package org.usfirst.frc.team1741.robot;
+
+import java.util.Scanner;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+
+public class FakePIDSource implements PIDSource
+{
+	private double m_value;
+	private double m_offset;
+	private double m_min;
+	private double m_max;
+	private double m_diff;
+	
+	FakePIDSource(double offset,double min,double max)
+	{
+		m_offset = offset;
+		m_min = min;
+		m_max = max;
+	}
+
+	@Override
+	public void setPIDSourceType(PIDSourceType pidSource) 
+	{
+		
+	}
+
+	@Override
+	public PIDSourceType getPIDSourceType() 
+	{
+		return PIDSourceType.kDisplacement;
+	}
+
+	@Override
+	public double pidGet() 
+	{
+		return m_value;
+	}
+	
+	public void pidSet(double value) 
+	{
+		m_value = value + m_offset;
+		if(m_value <= m_max && m_value >= m_min)//checks to see if within normal range
+		{
+			//nothing happens, pidGet will return a proper value 
+		}
+		else if(m_value > m_max)//checks if too high
+		{
+			m_diff = m_value - m_max;//get difference
+			m_value = m_min + m_diff;//sets value to wrap from max to min and add the diff
+		}
+		else if(m_value < m_min)//checks if too high
+		{
+			m_diff = m_min - m_value;//get difference
+			m_value = m_max - m_diff;//sets value to wrap from min to max and subtract the diff
+		}
+	}
+	
+	public void setOffset(double offset)
+	{
+		m_offset = offset;
+	}
+	
+	public void setMinMax(double min, double max)
+	{
+		m_min = min;
+		m_max = max;
+	}
+	
+    public static void main(String[] args) 
+    {
+    	FakePIDSource f = new FakePIDSource(-0.5,-5,5);;
+    	Scanner scan = new Scanner(System.in);
+    	double i = 0.0;
+        while(true)
+        {
+        	System.out.println("Please enter the value");
+        	i = scan.nextDouble();
+        	f.pidSet(i);
+        	System.out.println("PID Get: " + f.pidGet());
+        }
+    }	
+}
+
